4efaa4c8646b0dec7ed04ff651c26b308461ee60
Colin Greybosh <colingreybosh@gmail.com>
2025-11-03 18:36:20 -0800

    Solution for Day 4 Part 2

--- 2024/solutions/day04/solution.go
+++ 2024/solutions/day04/solution.go
@@ -2,10 +2,12 @@ package day04
 
 import (
 	"advent-of-code-2024/parsing"
+	"slices"
 )
 
 type Answer struct {
 	Part1 int64
+	Part2 int64
 }
 
 func Solution(path string) (Answer, error) {
@@ -13,11 +15,9 @@ func Solution(path string) (Answer, error) {
 	if err != nil {
 		return Answer{}, err
 	}
-	part1, err := countWords(wordSearch, []rune{'X', 'M', 'A', 'S'})
-	if err != nil {
-		return Answer{}, err
-	}
-	return Answer{Part1: part1}, nil
+	part1 := countWords(wordSearch, []rune{'X', 'M', 'A', 'S'})
+	part2 := countCrosses(wordSearch, []rune{'M', 'A', 'S'})
+	return Answer{Part1: part1, Part2: part2}, nil
 }
 
 type WordSearch struct {
@@ -26,14 +26,36 @@ type WordSearch struct {
 	height int
 }
 
-func countWords(wordSearch WordSearch, word []rune) (int64, error) {
+func countWords(wordSearch WordSearch, word []rune) int64 {
 	var count int64
 	for j := range wordSearch.height {
 		for i := range wordSearch.width {
 			count += countWordsInNeighborhood(wordSearch, word, i, j)
 		}
 	}
-	return count, nil
+	return count
+}
+
+func countCrosses(wordSearch WordSearch, word []rune) int64 {
+	var count int64
+	drow := make([]rune, len(word))
+	copy(drow, word)
+	slices.Reverse(drow)
+	for j := range wordSearch.height {
+		for i := range wordSearch.width {
+			if !(isWordInDirection(wordSearch, word, i, j, SOUTH_EAST) || isWordInDirection(wordSearch, drow, i, j, SOUTH_EAST)) {
+				// Did not find our word across the south east diagonal (neither forwards nor reversed)
+				continue
+			}
+			if !(isWordInDirection(wordSearch, word, i+len(word)-1, j, SOUTH_WEST) || isWordInDirection(wordSearch, drow, i+len(word)-1, j, SOUTH_WEST)) {
+				// Moved over to the top right corner of the square
+				// Did not find our work across the south west diagonal (neither forwards nor reversed)
+				continue
+			}
+			count++
+		}
+	}
+	return count
 }
 
 type Direction struct {
@@ -42,14 +64,14 @@ type Direction struct {
 }
 
 var (
-	NORTH      = Direction{deltaX: 0, deltaY: 1}
-	SOUTH      = Direction{deltaX: 0, deltaY: -1}
+	NORTH      = Direction{deltaX: 0, deltaY: -1}
+	SOUTH      = Direction{deltaX: 0, deltaY: 1}
 	EAST       = Direction{deltaX: 1, deltaY: 0}
 	WEST       = Direction{deltaX: -1, deltaY: 0}
-	NORTH_EAST = Direction{deltaX: 1, deltaY: 1}
-	NORTH_WEST = Direction{deltaX: -1, deltaY: 1}
-	SOUTH_EAST = Direction{deltaX: 1, deltaY: -1}
-	SOUTH_WEST = Direction{deltaX: -1, deltaY: -1}
+	NORTH_EAST = Direction{deltaX: 1, deltaY: -1}
+	NORTH_WEST = Direction{deltaX: -1, deltaY: -1}
+	SOUTH_EAST = Direction{deltaX: 1, deltaY: 1}
+	SOUTH_WEST = Direction{deltaX: -1, deltaY: 1}
 )
 
 var directions = []Direction{NORTH, SOUTH, EAST, WEST, NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_WEST}
